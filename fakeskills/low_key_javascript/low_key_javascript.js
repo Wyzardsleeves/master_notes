7597744164267cf82dd94e0062c1d86235f9b41c
Justin Lattimore <jlattimoreweb@gmail.com>
2022-08-14 19:25:53 -0500

    Added more notes for Destructured Assignments

--- fakeskills/low_key_javascript/low_key_javascript.js
+++ fakeskills/low_key_javascript/low_key_javascript.js
@@ -33,13 +33,13 @@
         console.log(camper)     //returns David
 
         let camper = "James";
-        let camper = "David";   //causes error
+        let camper = "David";   //redeclaration causes error
         camper = "David";       //reassigns, will not cause error
         console.log(camper);  
 
         const camper = "James"
-        const camper = "Lloyd"  //causes error
-        camper = "Lloyd"        //causes error
+        const camper = "Lloyd"  //redeclaration causes error
+        camper = "Lloyd"        //reassignment causes error
         console.log(camper);
 //      ---------------------------------------
 
@@ -93,13 +93,59 @@
         argCount(1, 4, "mouse", [40, 19])       //will return "You passed 4 arguments"
 //      --------------------------------------
 
-/*
-- Spread Operator
-        - Allows developer to expand arrays and other expressions in places where multiple parameters or elements are expected.
-        - Returns an unpacked array.
-        - Only works in-place, like an argument to a function.       
-*/
+
+// - Spread Operator
+//         - Allows developer to expand arrays and other expressions in places where multiple parameters or elements are expected.
+//         - Returns an unpacked array.
+//         - Only works in-place, like an argument to a function.       
+        
+//      -EXAMPLE------------------------------
+        let dogs = ["Great Dane", "Cocker"]
+        let newDogs = [...dogs]
+        console.log(newDogs)    //returns ['Great Dane', 'Cocker']
+//      --------------------------------------
+
+
+// - Destructuring Assignment
+//         - ES6 syntax for neatly assigning values taken directly from an object.
+//         - Removes the need for multiple assignments.
+//         - Also allows you to assign NEW names from old values.
+//         - Also works with nested objects.
+//         - Can also assign variables from Arrays
+//              - Different from a spread operator
+//              - Allows you to pick which elements you want to assign to variables
 
 //      -EXAMPLE------------------------------
+        const dogs2 = {breed: "Cocker", size: "medium"}
         
+        //Old Way (ES5)
+        var breed = dogs2.breed;
+        var size = dogs2.size
+        console.log(`A ${breed} is of ${size} size compared other dogs`)        //returns "A Cocker is of Medium size compared to other dogs"
+
+        //ES6 Assignment via Destructuring
+        const {breed, size} = dogs2;    //same thing with only one assignment
+        console.log(`A ${breed} is of ${size} size compared other dogs`)        //also returns "A Cocker is of Medium size compared to other dogs"
+
+        //Assignment with new variable names
+        const {breed: newBreedName, size: newSizeName} = dogs2
+        console.log(`A ${newBreedName} is of ${newSizeName} size compared other dogs`)     //returns "A Cocker is of Medium size compared to other dogs"
+
+        //Nested assignment
+        const cat = {
+                calico: {
+                        age: 8,
+                        name: "Niko" 
+                }
+        }
+
+        const {calico: {age: newAge, name: newName}} = cat;
+        console.log(`An ${newAge} year-old cat sat in a window. Her tag reads: ${newName}`)
+
+        //Assign variables from Arrays (different from spread operator)
+        const [a, b] = [1, 2, 3, 4, 5, 6];      
+        console.log(a, b)                       //returns 1, 2
+
+        const [a, b,,, c] = [1, 2, 3, 4, 5, 6]; //skipping assignments is allowed using commas to get to the index
+        console.log(a, b, c)                    //returns 1, 2, 5
 //      --------------------------------------
